0a6551063234a2ea41dd9803ab4704dbbd23870f
Kiyeon Roh <107321721+KYEON2@users.noreply.github.com>
2025-01-17 03:01:04 +0900

    Title: 사칙연산 함수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250117/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,33 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+def plus(x, y):
+    return x + y
+
+def minus(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+if o == "+":
+    result = plus(a, c)
+    print(f"{a} + {c} = {result}")
+elif o == "-":
+    result = minus(a, c)
+    print(f"{a} - {c} = {result}")
+elif o == "/":
+    result = div(a, c)
+    if result == "Error":
+        print(result)
+    else:
+        print(f"{a} / {c} = {result}")
+elif o == "*":
+    result = mul(a, c)
+    print(f"{a} * {c} = {result}")
+else:
+    print("False")
