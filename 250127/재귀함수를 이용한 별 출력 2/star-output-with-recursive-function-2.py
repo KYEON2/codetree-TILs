3833c9fc363645beed68e04f15a780bdeccff7d0
Kiyeon Roh <107321721+KYEON2@users.noreply.github.com>
2025-01-27 04:13:18 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
+++ 250127/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -1,12 +1,13 @@
 n = int(input())
 
 
-def printS (n):
+def printS(n):
     if n <= 0:  
         return
-    print("* " * n.rstrip())
+    print(" ".join(["*"] * n))  # 줄 끝 공백 제거
 
-    printS(n-1)
-    print("* " * n.rstrip())
+    printS(n - 1)
+    print(" ".join(["*"] * n))  # 줄 끝 공백 제거
 
-printS(5)
+
+printS(n)
